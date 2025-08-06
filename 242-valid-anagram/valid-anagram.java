a877425648d97a198d0ed74161595cb86ba9506d
Harinaath b <harinaath.b2024@vitstudent.ac.in>
2025-08-06 22:31:16 +0530

    Time: 69 ms (6.10%) | Memory: 55.1 MB (6.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
+        String[] a=s.split("");
+        String[] b=t.split("");
         Arrays.sort(a);
         Arrays.sort(b);
         return Arrays.equals(a,b);
