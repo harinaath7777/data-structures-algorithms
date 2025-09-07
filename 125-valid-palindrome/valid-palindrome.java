45e30ff06ce2ed77ad934ea6452d56484c9f052f
Harinaath b <harinaath.b2024@vitstudent.ac.in>
2025-09-07 22:55:24 +0530

    Time: 13 ms (42.23%) | Memory: 44.9 MB (35.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-        return cleaned.equals(reversed);
+        s=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
