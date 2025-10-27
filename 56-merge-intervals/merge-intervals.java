b3b3635ec4c7c56567dbc691f577f06edc741a9c
Harinaath b <harinaath.b2024@vitstudent.ac.in>
2025-10-27 23:47:42 +0530

    Time: 8 ms (84.98%) | Memory: 47.1 MB (9.77%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,23 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-            // Sort intervals based on the start time
-                    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-                            
-                                    List<int[]> ans = new ArrayList<>();
-                                            
-                                                    for (int[] interval : intervals) {
-                                                                // If list is empty or current interval does not overlap
-                                                                            if (ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]) {
-                                                                                            ans.add(interval);
-                                                                                                        } else {
-                                                                                                                        // Overlap case: merge the intervals
-                                                                                                                                        ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], interval[1]);
-                                                                                                                                                    }
-                                                                                                                                                            }
-                                                                                                                                                                    
-                                                                                                                                                                            // Convert list to array
-                                                                                                                                                                                    return ans.toArray(new int[ans.size()][]);
-                                                                                                                                                                                        }
-}                                                                                                                                                                                      
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]> ans=new ArrayList<>();
+        for(int[] interval:intervals){
+            if(ans.isEmpty() || interval[0]> ans.get(ans.size()-1)[1]){
+                ans.add(interval);
+            }
+            else{
+                ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],interval[1]);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
