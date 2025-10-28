3ba55127be94bc4cb33af32fd54f4ebca3930b00
Harinaath b <harinaath.b2024@vitstudent.ac.in>
2025-10-28 23:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (44.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //harinaath;
-        int i;
-        for(i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
+        int p1=m-1;
+        int p2=n-1;
+        int i=m+n-1;
+        while(p2>=0){
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[i]=nums1[p1];
+                i--;
+                p1--;
+            }
+            else{
+                nums1[i]=nums2[p2];
+                i--;
+                p2--;
+            }
         }
-        Arrays.sort(nums1);
     }
 }
