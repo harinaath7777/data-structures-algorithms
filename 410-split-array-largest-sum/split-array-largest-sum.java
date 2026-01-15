55bb8fbac4422d0a5d9e427180f4944555edfa87
Harinaath b <harinaath.b2024@vitstudent.ac.in>
2026-01-15 11:56:34 +0530

    Time: 1 ms (55.14%) | Memory: 43.1 MB (38.31%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    private int countSubarrays(int[] nums, int maxSum) {
+        int subarrays = 1;
+        int sum = 0;
+
+        for (int x : nums) {
+            if (sum + x <= maxSum) {
+                sum += x;
+            } else {
+                subarrays++;
+                sum = x;
+            }
+        }
+        return subarrays;
+    }
+
+    public int splitArray(int[] nums, int k) {
+
+        int low = 0, high = 0;
+
+        // low = max element, high = sum of array
+        for (int x : nums) {
+            low = Math.max(low, x);
+            high += x;
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int subarrays = countSubarrays(nums, mid);
+
+            if (subarrays > k) {
+                low = mid + 1;      // mid too small
+            } else {
+                high = mid - 1;    // try smaller answer
+            }
+        }
+
+        return low;
+    }
+}
